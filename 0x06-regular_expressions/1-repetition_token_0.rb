22c76d434b1c57f0b2d667349508020a4ae74a8e
Kimulu <kimulumichael@gmail.com>
2024-02-29 22:46:40 -0800

    new commit

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-# Script: 1-repetition_token_0.rb
-# Description: accepts one argument and checks if it matches a specific pattern
-# The regular expression matches strings that start with "hb", followed by 1 to 5 occurrences of the letter "t"
-
-regex = /hbt{2,5}n/
-
-input_string = ARGV[0]
-
-if input_string =~ regex
-  puts "Match found: #{input_string}"
-else
-  puts "No match found."
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
